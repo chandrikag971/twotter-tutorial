714c644525e194010e0c07be75c475f90e5ab229
chandrikag971 <chandrikag971@gmail.com>
2021-05-08 13:07:35 +0530

    Added two files

--- twotter-tutorial/src/App.vue
+++ twotter-tutorial/src/App.vue
@@ -8,19 +8,18 @@
 import Userprofile from "./components/Userprofile";
 
 export default {
-  name: 'App',
-  components:{Userprofile}
-    
+	name: 'App',
+	components:{ Userprofile }   
 }
 </script>
 
-<style>
+<style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
-  min-height:100vh;
-  background-color:#F3F5FA;
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	color: #2c3e50;
+	min-height:100vh;
+	background-color:#F3F5FA;
 }
 </style>
