3a6a41fa4de58bbd7f9ce08bf592207e5fa6c56e
chandrikag971 <chandrikag971@gmail.com>
2021-05-10 08:57:11 +0530

    routing with router vue

---
+++ twotter-tutorial/src/assets/users.js
@@ -0,0 +1,41 @@
+export const users = [
+	{
+		"id":1,
+		"username":"_Chandrika",
+		"firstName":"Chandrika",
+		"lastName": "G",
+		"email": "chandrikag971@gmail.com",
+		"isAdmin": true,
+		"twoots": [
+			{ "id":1,"content":"This is a vue framework"},
+			{ "id":2,"content":"It is used for developing websites"}
+		]
+	},
+	{
+		"id":2,
+		"username":"_Manga",
+		"firstName":"Manga",
+		"lastName": "G",
+		"email": null,
+		"isAdmin": false,
+		"twoots": []
+	},
+	{
+		"id":3,
+		"username":"_Eswararao",
+		"firstName":"Eswararao",
+		"lastName": "G",
+		"email": null,
+		"isAdmin": false,
+		"twoots": []
+	},
+	{
+		"id":4,
+		"username":"_Kumar",
+		"firstName":"Kumar",
+		"lastName": "G",
+		"email": null,
+		"isAdmin": false ,
+		"twoots": []
+	},
+]
